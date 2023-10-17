7aba04c686689bba0526ada2c3f6ca9c1c28d859
HitchedLime <71065096+HitchedLime@users.noreply.github.com>
2023-10-17 21:47:06 +0200

    added drop  off after model stop improving after few epochs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 import torch
 import torch.nn as nn
@@ -12,19 +13,25 @@ dataset = pd.read_csv("HeartDisease.csv")
 
 X = dataset.drop(['row.names', 'chd'], axis=1)
 y = dataset['chd']
-X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.33, random_state=42)
+scaler = preprocessing.StandardScaler().fit(X.values)
+X = scaler.transform(X.values)
+X_train, X_test, y_train, y_test = train_test_split(X, y.values, test_size=0.33, random_state=42)
 
 
 net = neural_net.NeuralNetwork()
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
 
-batch_size = 100
-num_epochs = 5
+batch_size = 5
+num_epochs = 15
 learning_rate = 0.001
 batch_no = len(X_train) // batch_size
+best_loss = np.inf
+n_epochs_stop = 2
+epochs_no_improve = 0
 
 for epoch in range(num_epochs):
+    total_loss = 0
     if epoch % 5 == 0:
         print('Epoch {}'.format(epoch + 1))
     x_train, y_train = shuffle(X_train, y_train)
@@ -38,10 +45,24 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()
         ypred_var = net(x_var)
         loss = criterion(ypred_var, y_var)
-        print(f"Training Loss : {loss}")
+        total_loss +=loss
+
+
         loss.backward()
         optimizer.step()
+        total_loss += loss.item()
+    avg_loss = total_loss / batch_no
+    print(f"Training Loss : {avg_loss}")
 
+
+    if(avg_loss < best_loss):
+            best_loss = avg_loss
+            epochs_no_improve = 0
+    else:
+            epochs_no_improve += 1
+            if epochs_no_improve == n_epochs_stop:
+                print('Early stopping!')
+                break
 test_var = Variable(torch.FloatTensor(X_test), requires_grad=True)
 
 with torch.no_grad():
