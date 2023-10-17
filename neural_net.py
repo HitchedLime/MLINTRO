1fc4fb1a59228cc8e2222e53efbf3f7131934334
HitchedLime <71065096+HitchedLime@users.noreply.github.com>
2023-10-17 21:47:39 +0200

    added normalization and drop out
    added

--- neural_net.py
+++ neural_net.py
@@ -1,3 +1,5 @@
+import random
+
 from torch import nn
 
 
@@ -6,19 +8,33 @@ class NeuralNetwork(nn.Module):
         super().__init__()
 
         self.linear_relu_stack = nn.Sequential(
-            nn.Linear(9, 50),
+            nn.Linear(9, 15),
             nn.ReLU(),
-            nn.Linear(50, 50),
+            nn.InstanceNorm1d(15),
+            nn.Linear(15, 15),
+            nn.Dropout(random.uniform(0,1)),
             nn.ReLU(),
-            nn.Linear(50, 50),
+            nn.InstanceNorm1d(15),
+
+
+        )
+
+
+
+        self.linear_relu_stack_2 = nn.Sequential(
+
+            nn.Linear(15, 15),
+            nn.InstanceNorm1d(15),
             nn.ReLU(),
-            nn.Linear(50, 50),
+            nn.Linear(15, 15),
+            nn.InstanceNorm1d(15),
             nn.ReLU(),
-            nn.Linear(50, 2),
+            nn.Linear(15, 2),
         )
 
     def forward(self, x):
+        x = self.linear_relu_stack(x)
 
-        logits = self.linear_relu_stack(x)
+
+        logits = self.linear_relu_stack_2(x)
         return logits
-
